bb3963ee09eaaee9c348590c324b013239d4d81c
P0645300 <rodrigo.magalhaes@portoseguro.com.br>
2020-07-31 18:55:10 -0300

    Update object mongo atlas

--- src/controllers/VendaController.js
+++ src/controllers/VendaController.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const Venda =  mongoose.model('Venda');
 const Pessoa =  mongoose.model('Pessoa'); 
+const { ObjectID } = require('mongodb');
 
 module.exports = {
 
@@ -26,30 +27,39 @@ module.exports = {
     },
 
     async salvar(req, res) {
+        console.info("Salvar")
         const reqVenda = req.body;
         if (reqVenda != null) {
-            let venda = {};
-            //TODO ARRUMAR ISSO
-            venda = await preparaObjetos(reqVenda)
-                .then(
-                    function(result){
-                        console.log("Promise resolved: " + result);
-                        return result;
-                    })
-                .catch(function(error){
-                    console.log("Promise resolved: " + error);
-                    return null;
-                });
-            
-            console.log(venda._id + " " + venda.vitrine + " Preparado com sucesso.")
-                
-            // {
-            //     if (err) return console.error("Deu erro aqui -->> \n" + err);
-            //     console.log(vendaCallback._id + " " + vendaCallback.vitrine + " Preparado com sucesso.");
-            //     venda = vendaCallback;
-            // });
-            const pessoa =  venda.cliente;
+            const venda = new Venda();
+            const estrela = {};
+            const pessoa = new Pessoa();
+            console.info("Requisicao: " + JSON.stringify(reqVenda))
 
+            pessoa._id = reqVenda.idCliente;
+            pessoa.nome = reqVenda.nome;
+            pessoa.telefone = reqVenda.telefone.replace(/\D/g, "");;
+
+            var pesoEstrela = 0
+            if (reqVenda.dobroEstrela) {
+                console.info("Requisicao com dobro de estrelas");
+                pesoEstrela = 37, 5
+            } else {
+                console.info("Requisicao sem dobro de estrelas");
+                pesoEstrela = 75
+            }
+
+            if (reqVenda.valor != null && reqVenda.valor > 0) {
+                estrela.qtd = reqVenda.valor / pesoEstrela;
+                venda.valor = reqVenda.valor;
+            }
+            estrela.usado = false;
+            estrela.quandoUsado = null;
+            venda.dobro = reqVenda.dobroEstrela;
+            venda.vitrine = reqVenda.vitrine;
+            venda.estrela = estrela;
+            venda.cliente = pessoa;
+
+            console.log(venda._id + " " + venda.vitrine + " Preparado com sucesso.");
             console.info("Salvando " + JSON.stringify(pessoa));
 
             if (pessoa._id) {
@@ -65,26 +75,70 @@ module.exports = {
             }
             
             console.info("Salvando " + JSON.stringify(venda)); 
-
-            if (venda._id) {
-                venda.updateOne({_id: venda._id}, function (err) {
-                    if (err) return console.error("Deu erro aqui -->> \n" + err);
-                    console.log(venda._id + " " + venda.vitrine + " Salvo com sucesso.");
-                });
-            } else {
-                venda.save(function (err) {
-                    if (err) return console.error("Deu erro aqui -->> \n" + err);
-                    console.log(venda._id + " " + venda.vitrine + " Salvo com sucesso.");
-                });
-            }
-
+            venda.save(function (err) {
+                if (err) return console.error("Deu erro aqui -->> \n" + err);
+                console.log(venda._id + " " + venda.vitrine + " Salvo com sucesso.");
+            });
+ 
             return res.json(venda); 
         }
     },
  
     async atualizar(req, res) {
-        const venda = await Venda.findByIdAndUpdate(req.params.id, req.body, {new : true});
-        return res.json(venda); 
+        console.info("Atualizar " + req.params.id);
+        const reqVenda = req.body;
+        if (reqVenda != null) {
+            
+            // await Venda.findById(new ObjectID(req.params.id), function (err, venda) {
+            const venda = new Venda();    
+            const estrela = {};
+            const pessoa = new Pessoa();
+            console.info("Requisicao: " + JSON.stringify(reqVenda))
+
+            // if (err) return console.error("Deu erro aqui -->> \n" + err);
+            console.log("Find by id " + venda);                
+            
+            pessoa._id = reqVenda.idCliente;
+            pessoa.nome = reqVenda.nome;
+            pessoa.telefone = reqVenda.telefone.replace(/\D/g, "");;
+            
+            var pesoEstrela = 0
+            if (reqVenda.dobroEstrela) {
+                console.info("Requisicao com dobro de estrelas");
+                pesoEstrela = 37, 5
+            } else {
+                console.info("Requisicao sem dobro de estrelas");
+                pesoEstrela = 75
+            }
+            
+            if (reqVenda.valor != null && reqVenda.valor > 0) {
+                estrela.qtd = reqVenda.valor / pesoEstrela;
+                venda.valor = reqVenda.valor;
+            }
+            estrela.usado = false;
+            estrela.quandoUsado = null;
+            venda.dobro = reqVenda.dobroEstrela;
+            venda.vitrine = reqVenda.vitrine;
+            venda.estrela = estrela;
+            venda.cliente = pessoa;
+            
+            console.log(venda._id + " " + venda.vitrine + " Preparado com sucesso.");
+            console.info("Atualizando " + JSON.stringify(pessoa));
+            pessoa.updateOne({_id : pessoa._id}, function (err) {
+                if (err) return console.error("Deu erro aqui -->> \n" + err);
+                console.log(pessoa._id + " " + pessoa.nome + " Salvo com sucesso.");
+            });
+            
+            console.info("Atualizar " + JSON.stringify(venda)); 
+            venda._id = new ObjectID(req.params.id)
+            Venda.updateOne({_id: req.params.id}, venda, function (err) {
+                if (err) return console.error("Deu erro aqui -->> \n" + err);
+                console.log(req.params.id + " " + venda.vitrine + " Atualizado com sucesso.");
+            });
+            
+            return res.json("venda1");
+            // });
+        }
     },
 
     async remover(req, res) {
@@ -96,60 +150,3 @@ module.exports = {
     }
     
 };
-
-var preparaObjetos = async function(reqVenda) {
-    //TODO VEJA SE ESTÁ CAINDO EM ALGUM ERRO AQUI   
-    const promise = new Promise( (resolve, reject) => {
-        try {
-            const venda = new Venda();
-            const estrela = {};
-            const pessoa = new Pessoa();
-            console.info("Requisicao: " + JSON.stringify(reqVenda))
-
-            pessoa._id = reqVenda.idCliente;
-            pessoa.nome = reqVenda.nome;
-            pessoa.telefone = reqVenda.telefone.replace(/\D/g,"");;
-
-            var pesoEstrela = 0
-            if (reqVenda.dobroEstrela) {
-                pesoEstrela = 37,5
-            } else {
-                pesoEstrela = 75
-            }
-
-            if (reqVenda.valor != null && reqVenda.valor > 0) {
-                estrela.qtd = reqVenda.valor / pesoEstrela;
-                venda.valor = reqVenda.valor;
-            }
-
-            estrela.usado=false;
-            estrela.quandoUsado=null;
-
-            venda._id = reqVenda.id;
-            venda.dobro = reqVenda.dobroEstrela;
-            venda.vitrine = reqVenda.vitrine;
-            venda.estrela = estrela;
-            
-            venda.cliente = pessoa;
-            resolve(venda);
-        } catch(error) {
-            console.error("Ocorreu um erro na preparacao " + error)
-            reject(error);
-        }
-    });
-    return promise;
-};
-
-//index.js
-function soAceitaPares(numero){
-    const promise = new Promise( (resolve, reject) => { 
-                        if (numero % 2 === 0) { 
-                            const resultado = 'Viva, é par!';
-                            resolve(resultado);
-                        } 
-                        else { 
-                            reject(new Error("Você passou um número ímpar!"));
-                        } 
-                    });
-    return promise;
-}
